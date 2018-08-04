bc6ff8b4b162d4f0795215ab9dbbab61844bb2fd
rorschach.h <yiming.whz@gmail.com>
2018-08-04 20:43:12 +0800

    update.

--- shell/install/install_git.sh
+++ shell/install/install_git.sh
@@ -2,22 +2,50 @@
 # install and config git on ubuntu.
 sudo apt update
 
-sudo apt install -y git
-
-read -r -t 60 -p "do you want to set git config[Y/n]:" response
-response=${response,,}
-if [[ $response == "y" ]] 
-then
-    echo "all configuration will be set as global"
-    read -r -t 60 -p "enter your user name: " username
-    git config --global user.name $username
-    read -r -t 60 -p "enter your email: " email
-    git config --global user.email $email
-    read -r -t 60 -p "enter your core editor: " editor
-    git config --global core.editor $editor
-		git config --global core.autocrlf input
-    git config -l
+echo ""
+if [[ -z $(which git) ]]; then
+	sudo apt install -y git
 fi
 
-version=`git --version`    
+echo ""
+read -r -t 60 -p "do you want to set git config[Y/n]:" response
+response=${response,,}
+if [[ $response == "y" ]]; then
+	echo "all configuration will be set as global"
+	read -r -t 60 -p "enter your user name: " username
+	if [[ -n $username ]]; then
+		git config --global user.name $username
+	fi
+	read -r -t 60 -p "enter your email: " email
+	if [[ -n $email ]]; then
+		git config --global user.email $email
+		echo ""
+	fi
+	read -r -t 60 -p "enter your core editor: " editor
+	if [[ -n $email ]]; then
+		git config --global core.editor $editor
+		echo ""
+	fi
+	read -r -t 60 -p "enter your http proxy host: " host
+	echo ""
+	read -r -t 60 -p "enter your http proxy port: " port
+	echo ""
+	read -r -t 60 -p "enter your sock5 proxy port: " sock5port
+	if [[ -n $host && -n $port ]]; then
+		git config --global http.https://github.com.proxy  http://$host:$port
+		git config --global https.https://github.com.proxy https://$host:$port
+	fi
+	if [[ -n $host && -n $sock5port ]]; then
+		git config --global http.https://github.com.proxy  http://$host:$sock5port
+		git config --global https.https://github.com.proxy https://$host:$sock5port
+	fi
+	git config --global core.autocrlf input
+	git config --global http.postBuffer 524288000
+	echo ""
+	echo "######################################################"
+	git config -l
+fi
+
+echo "######################################################"
+version=`git --version`
 echo "installed $version"
--- shell/install/install_vim.sh
+++ shell/install/install_vim.sh
@@ -11,6 +11,6 @@ sudo apt install libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
 	python3-dev ruby-dev mercurial
-	
+
 sudo apt install vim -y
 
