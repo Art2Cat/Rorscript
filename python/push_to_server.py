4e80f9520db1d435b9ad186628762db0231f9940
rorschach.h <yiming.whz@gmail.com>
2018-11-13 17:52:26 +0800

    update .ideavimrc and push_to_server.py

--- python/push_to_server.py
+++ python/push_to_server.py
@@ -1,11 +1,11 @@
 import asyncio
 import os
+import shutil
 import subprocess
 import sys
 from datetime import datetime
-from zipfile import ZipFile
 
-from paramiko import SSHClient
+from paramiko import SSHClient, WarningPolicy
 from scp import SCPClient
 
 
@@ -22,31 +22,6 @@ class SSHInfo(object):
 
     def __str__(self):
         return "SSHInfo(hostname={!r}, port={!r}, username={!r}, password={!r})".format(*self)
-
-
-def get_all_file_paths(directory):
-    file_paths = []
-
-    for root, directories, files in os.walk(directory):
-        for filename in files:
-            filepath = os.path.join(root, filename)
-            if "target" not in filepath or ".svn" not in filepath:
-                file_paths.append(filepath)
-
-    return file_paths
-
-
-def create_zip(zip_file_path: str, directory: str):
-    file_paths = get_all_file_paths(directory)
-
-    print('Following files will be zipped:')
-
-    with ZipFile(zip_file_path, 'w') as zip:
-        for file in file_paths:
-            print(os.path.basename(file))
-            zip.write(file)
-
-    print('All files zipped successfully!')
 
 
 def execute(command):
@@ -84,11 +59,11 @@ async def push(source_path: str, ssh: SSHClient):
                          (filename, float(sent) / float(size) * 100))
 
     with SCPClient(ssh.get_transport(), progress=progress) as scp:
-        await scp.put("code.zip", source_path)
+        await scp.put(source_path, source_path)
 
 
-async def remote_unzip(ssh: SSHClient, target_dir: str):
-    cmd = "unzip code.zip -d {} && rm code.zip".format(target_dir)
+async def remote_unzip(ssh: SSHClient, source_path: str, target_dir: str):
+    cmd = "unzip {} -d {} && rm {}".format(source_path, target_dir, source_path)
     await ssh.exec_command(cmd)
 
 
@@ -102,34 +77,41 @@ async def remote_deploy(ssh: SSHClient, target_dir: str):
     await ssh.exec_command(cmd)
 
 
-def main():
-    create_zip("", ".")
-    loop = asyncio.get_event_loop()
+def main(loop, filename):
+    zip_file_path = os.path.join(current_dir, filename)
+    shutil.make_archive(filename, 'zip', zip_file_path)
 
     ssh_info = SSHInfo()
     ssh = SSHClient()
+    ssh.load_system_host_keys()
+    ssh.set_missing_host_key_policy(WarningPolicy())
     ssh.connect(*ssh_info)
-    remote_tasks = [push("", ssh), remote_unzip(ssh, "")]
+    target_path = "/home/lp_app/lp_live/{}".format(filename)
+    remote_tasks = [push("{}.zip".format(filename), ssh), remote_unzip(ssh, "{}.zip".format(filename), target_path)]
     futures = [asyncio.ensure_future(t, loop=loop) for t in remote_tasks]
     gathered = asyncio.gather(*futures, loop=loop, return_exceptions=True)
     loop.run_until_complete(gathered)
-
-    for fut in futures:
-        try:
-            yield fut.result()
-        except Exception as e:
-            yield repr(e)
+    #
+    # for fut in futures:
+    #     try:
+    #         yield fut.result()
+    #     except Exception as e:
+    #         yield repr(e)
 
 
 if __name__ == "__main__":
     start_time = datetime.now()
-    if len(sys.argv) < 2:
-        print("use the script like: %s code_dir remote_host target_dir" %
+    if len(sys.argv) < 1:
+        print("use the script like: %s code_dir" %
               sys.argv[0])
         sys.exit()
 
-    source_dir = sys.argv[1]
-    main()
+    current_dir = os.path.abspath(".")
+    filename = sys.argv[1]
+    loop = asyncio.get_event_loop()
+    main(loop, filename)
+    # # asyncio.run(main(loop))
+
     print("done")
     end_time = datetime.now()
     total = end_time - start_time
