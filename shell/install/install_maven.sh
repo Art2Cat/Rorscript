75909f7a89129ec68cb6bf737c8c009cc776e3b8
art2cat <yiming.whz@gmail.com>
2019-01-06 22:45:04 -0500

    add monitor_swap.sh

--- shell/install/install_maven.sh
+++ shell/install/install_maven.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # install maven on ubuntu.
-wget http://www.eu.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
-tar -xvzf apache-maven-3.5.2-bin.tar.gz
+wget http://www.eu.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
+tar -xvzf apache-maven-3.5.4-bin.tar.gz
 sudo mkdir /usr/local/maven
-sudo mv apache-maven-3.5.2/ /usr/local/maven/
-sudo update-alternatives --install /usr/bin/mvn mvn /usr/local/maven/apache-maven-3.5.2/bin/mvn 1
+sudo mv apache-maven-3.5.4/ /usr/local/maven/
+sudo update-alternatives --install /usr/bin/mvn mvn /usr/local/maven/apache-maven-3.5.4/bin/mvn 1
 sudo update-alternatives --config mvn
-rm apache-maven-3.5.2-bin.tar.gz
+rm apache-maven-3.5.4-bin.tar.gz
---
+++ shell/monitor_swap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# reboot server when swap less then 20mb.
+while true;
+do
+
+	USAGE=`free -m | awk 'NR==3{print $4}'`
+	if [ $USAGE -lt 20 ]; then
+		reboot
+		fi
+	done
