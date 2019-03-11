fdbe6b050d6c647248e90a7d93a549d6c44de701
Rorschach H <yiming.whz@gmail.com>
2019-03-11 16:26:32 +0800

    update monitor_swap.sh

--- shell/monitor_swap.sh
+++ shell/monitor_swap.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+# add script to crontab
 # reboot server when swap less then 20mb.
-while true;
-do
 
-	USAGE=`free -m | awk 'NR==3{print $4}'`
-	if [ $USAGE -lt 20 ]; then
-		reboot
-	fi
-done
+
+COMMAND='59 23 * * * root ~/Rorscipt/shell/monitor_swap.sh'
+
+EXISTS=`cat /etc/crontab | grep monitor_swap.sh`
+
+if [ $EXISTS -z ];
+then
+	echo -e $COMMAND >> /etc/crontab
+fi
+
+
+USAGE=`free -m | awk 'NR==3{print $4}'`
+if [ $USAGE -lt 20 ]; then
+	reboot
+fi
